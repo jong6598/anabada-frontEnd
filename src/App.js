f73ce3f20c9cb47404f2b93b0459f605098e700f
FerrariRoma <win9592@gmail.com>
2022-08-14 00:46:53 +0900

    kakao map 커스텀 성공

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import { ThemeProvider } from 'styled-components';
-import { Routes, Route } from 'react-router-dom';
-import theme from './styles/theme';
+import "./App.css";
+import { ThemeProvider } from "styled-components";
+import { Routes, Route } from "react-router-dom";
+import theme from "./styles/theme";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
         <Routes>
-          <Route></Route>
+          <Route path="/" element={<Home />} />
         </Routes>
       </ThemeProvider>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Map from "../components/Map";
+
+const Home = () => {
+  return (
+    <>
+      <Map />
+    </>
+  );
+};
+
+export default Home;
