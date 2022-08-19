434ce321a2a662682897dc4b3b79363c04a6c046
FerrariRoma <win9592@gmail.com>
2022-08-20 04:29:32 +0900

    로그인 기본 CSS 작업 완료

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,11 @@
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { userAuth } from "../shared/api";
 import { Cookies } from "react-cookie";
 import { userThunk } from "../redux/auth-slice";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import styled from "styled-components";
 
 const Login = () => {
   const {
@@ -50,33 +51,153 @@ const Login = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSumbit, onError)}>
-        <input
-          type="email"
-          placeholder="이메일"
-          {...register("email", {
-            required: "이메일을 입력해주세요!",
-            pattern: /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/,
-          })}
-        ></input>
-        {errors.email?.type === "required" && (
-          <span>{errors.email.message}</span>
-        )}
-        {errors.email?.type === "pattern" && (
-          <span>형식에 맞게 메일 주소를 입력하세요.</span>
-        )}
-        <input
-          type="password"
-          placeholder="비밀번호"
-          {...register("password", { required: "비밀번호를 입력해주세요!" })}
-        ></input>
-        {errors.password?.type === "required" && (
-          <span>{errors.password.message}</span>
-        )}
-        <button>로그인</button>
-      </form>
+      <FormWrapper className="login__wrapper">
+        <section className="login__wrapper-welcome__message">
+          <h1>환영합니다!</h1>
+          <br />
+          <h1>로그인을 해주세요.</h1>
+        </section>
+        <FormDiv>
+          <form
+            className="login__wrapper-form"
+            onSubmit={handleSubmit(onSumbit, onError)}
+          >
+            <input
+              className="login__wrapper-input login__wrapper-input__login"
+              type="email"
+              placeholder="이메일"
+              {...register("email", {
+                required: "이메일을 입력해주세요!",
+                pattern: /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/,
+              })}
+            ></input>
+            {/* {errors.email?.type === "required" && (
+              <span className="login__wrapper-error">
+                {errors.email.message}
+              </span>
+            )}
+            {errors.email?.type === "pattern" && (
+              <span className="login__wrapper-error">
+                형식에 맞게 메일 주소를 입력하세요.
+              </span>
+            )} */}
+            <input
+              className="login__wrapper-input login__wrapper-input__password"
+              type="password"
+              placeholder="비밀번호"
+              {...register("password", {
+                required: "비밀번호를 입력해주세요!",
+              })}
+            ></input>
+            {/* {errors.password?.type === "required" && (
+              <span className="login__wrapper-error">
+                {errors.password.message}
+              </span>
+            )} */}
+            <button className="login__wrapper-btn">로그인</button>
+          </form>
+          <section className="login__wrapper-extra">
+            <div className="login__wrapper-extra__btn__find">
+              <span className="login__wrapper-extra__btn">아이디 찾기</span>
+              <div className="header__user__divider">
+                <div></div>
+              </div>
+              <span className="login__wrapper-extra__btn">비밀번호 찾기</span>
+            </div>
+            <div className="login__wrapper-extra__btn__signup">
+              <Link to="/signup">
+                <span className="login__wrapper-extra__btn">회원가입</span>
+              </Link>
+            </div>
+          </section>
+        </FormDiv>
+      </FormWrapper>
     </>
   );
 };
 
 export default Login;
+
+const FormWrapper = styled.div`
+  margin-top: 8.375rem;
+  .login__wrapper-welcome__message {
+    text-align: center;
+    h1 {
+      margin: 0 auto;
+      font-weight: 400;
+      font-size: 1.5625rem;
+      color: #000000;
+    }
+    h1:last-child {
+      margin-top: -0.9rem;
+    }
+  }
+`;
+
+const FormDiv = styled.div`
+  margin-top: 2.1875rem;
+  .login__wrapper-form {
+    display: flex;
+    flex-direction: column;
+    .login__wrapper-input,
+    .login__wrapper-btn,
+    .login__wrapper-error {
+      width: 18.1875rem;
+      height: 2.5625rem;
+      margin: 0 auto;
+      border-radius: 0.3125rem;
+    }
+    .login__wrapper-input {
+      margin-bottom: 0.5rem;
+      background: #ffffff;
+      border: 1px solid #d1d1d6;
+      align-items: flex-start;
+      padding: 12px 10px;
+    }
+    .login__wrapper-btn {
+      margin-top: 1.6875rem;
+      margin-bottom: 1rem;
+      border: none;
+      background: #007aff;
+      color: white;
+      font-size: 1rem;
+      font-weight: 600;
+      cursor: pointer;
+    }
+  }
+  .login__wrapper-extra {
+    width: 18.1875rem;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+
+    font-weight: 400;
+    font-size: 13px;
+    color: #8e8e93;
+    .login__wrapper-extra__btn {
+      cursor: pointer;
+    }
+    .login__wrapper-extra__btn:hover {
+      color: black;
+    }
+
+    .login__wrapper-extra__btn__find {
+      display: flex;
+    }
+
+    // 헤더에 있는 거 가져옴
+    .header__user__divider {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 1rem;
+      height: 1rem;
+      div {
+        height: 0.5rem;
+        width: 1px;
+        border-radius: 1rem;
+        background-color: #c7c7cc;
+      }
+    }
+  }
+`;
